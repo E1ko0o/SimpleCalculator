71e3a380951e65c471fc0e93bd77801e43632c0a
E1ko0o <slava121231@mail.ru>
2022-04-23 21:54:29 +0300

    Move logic to viewModel from NumberFragment

---
+++ app/src/main/java/com/e1ko0o/android/simplecalculator/MainViewModel.kt
@@ -0,0 +1,95 @@
+package com.e1ko0o.android.simplecalculator
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class MainViewModel : ViewModel() {
+    val liveDataForResult = MutableLiveData<String>()
+    val liveDataForNumber = MutableLiveData<String>()
+
+    private var result: Double = 0.0
+    private var operation: String = ""
+    private var number: Double = 0.0
+
+    private fun doMath() {
+        when (operation.lowercase().trim()) {
+            "%" -> result *= number / 100
+            "^" -> result = result.pow(number.toInt())
+            "√" -> result = if (number.toString().contains("-")) 0.0 else sqrt(number)
+            "+" -> result += number
+            "-" -> result -= number
+            "*" -> result *= number
+            "/" -> result /= number
+            else -> result = number
+        }
+        if (result.toString().endsWith(".0"))
+            liveDataForNumber.value = result.toInt().toString()
+        else
+            liveDataForNumber.value = result.toString()
+    }
+
+    fun onDotClicked(dot: String) {
+        liveDataForResult.value += dot
+        liveDataForNumber.value += dot
+    }
+
+    fun onNumberClicked(number: Double) {
+        val ld = liveDataForResult.value.toString()
+        val buffer = ld.replace("[+\\-/%*^√(?!null)]".toRegex(), "") + number.toString()
+        if (buffer == number.toString()) {
+            if (buffer.endsWith(".0")) {
+                liveDataForResult.value += number.toInt().toString()
+                liveDataForNumber.value = number.toInt().toString()
+            } else {
+                liveDataForResult.value += number.toString()
+                liveDataForNumber.value = number.toString()
+            }
+        } else {
+            if (buffer.endsWith(".0")) {
+                liveDataForResult.value += number.toInt().toString()
+                liveDataForNumber.value += number.toInt().toString()
+            } else {
+                liveDataForResult.value += number.toString()
+                liveDataForNumber.value += number.toString()
+            }
+        }
+        this.number = number
+        doMath()
+    }
+
+    fun onOperationClicked(operation: String) {
+        if (operation == "√") {
+            liveDataForResult.value = operation + result.toString()
+            result = if (result.toString().dropLast(1) != "") result.toString().dropLast(1)
+                .toDouble() else 0.0
+        } else {
+            liveDataForResult.value += operation
+            result = if (result.toString().dropLast(1) != "") result.toString().dropLast(1)
+                .toDouble() else 0.0
+        }
+        liveDataForNumber.value = ""
+        this.operation = operation
+    }
+
+    fun onResultClicked() { //@todo test
+//        doMath()
+        number = result
+        if (number.toString().endsWith(".0")) {
+            liveDataForResult.value = result.toInt().toString()
+            liveDataForNumber.value = ""
+        } else {
+            liveDataForResult.value = result.toString()
+            liveDataForNumber.value = ""
+        }
+    }
+
+    fun onClearClicked() {
+        result = 0.0
+        number = 0.0
+        operation = ""
+        liveDataForResult.value = ""
+        liveDataForNumber.value = ""
+    }
+}
--- app/src/main/java/com/e1ko0o/android/simplecalculator/NumbersFragment.kt
+++ app/src/main/java/com/e1ko0o/android/simplecalculator/NumbersFragment.kt
@@ -5,11 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.e1ko0o.android.simplecalculator.databinding.FragmentNumbersLinearBinding
-import kotlin.math.pow
-import kotlin.math.sqrt
 
 private const val TAG = "LOL!"
 // @TODO Ты хорошо поработал, посмотри калькулятор гугла или windows на компе
@@ -17,115 +15,25 @@ private const val TAG = "LOL!"
 
 class NumbersFragment : Fragment(R.layout.fragment_numbers_constraint) {
     private lateinit var binding: FragmentNumbersLinearBinding
+    private lateinit var mainViewModel: MainViewModel
 
     private inner class Controller : View.OnClickListener {
         override fun onClick(v: View?) {
             val btn = v?.findViewById<Button>(v.id)
             when (val btnId = btn?.text.toString()) {
-                in "0".."9" -> onNumberClicked(btnId.toDouble())
-                "=" -> onResultClicked()
-                "C" -> onClearClicked()
-                "." -> onDotClicked(btnId)
-                else -> onOperationClicked(btnId)
+                in "0".."9" -> mainViewModel.onNumberClicked(btnId.toDouble())
+                "=" -> mainViewModel.onResultClicked()
+                "C" -> mainViewModel.onClearClicked()
+                "." -> mainViewModel.onDotClicked(btnId)
+                else -> mainViewModel.onOperationClicked(btnId)
             }
         }
-    }
-
-    private fun doMath() {
-        when (operation.lowercase().trim()) {
-            "%" -> result *=  number / 100
-            "^" -> result = result.pow(number.toInt())
-            "√" -> {
-                result = if (number.toString().contains("-")) {
-                    Toast.makeText(
-                        activity?.applicationContext,
-                        "Can't take the square root of a negative number",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    0.0
-                } else sqrt(number)
-            }
-            "+" -> result += number
-            "-" -> result -= number
-            "*" -> result *= number
-            "/" -> if (number.toString() != "0") result /= number
-            else {
-                Toast.makeText(
-                    activity?.applicationContext,
-                    "Can't divide by zero",
-                    Toast.LENGTH_SHORT
-                ).show()
-                result = 0.0
-            }
-        }
-    }
-
-    fun onDotClicked(operation: String) {
-        binding.tvResult.append(operation)
-        binding.tvNumber.append(operation)
-    }
-
-    fun onNumberClicked(number: Double) {
-        val buffer = binding.tvNumber.text.toString().replace(
-            "[+\\-/%*^√]".toRegex(), "") + number.toInt().toString()
-        if (buffer.isNotEmpty()) {
-            if (number.toString().endsWith(".0") || number.toString().endsWith(".")) {
-                binding.tvNumber.append(number.toInt().toString())
-                binding.tvResult.append(number.toInt().toString())
-            } else {
-                binding.tvNumber.append(number.toString())
-                binding.tvResult.append(number.toString())
-            }
-            NumbersFragment.number = buffer.toDouble()
-        } else {
-            if (number.toString().endsWith(".0") || number.toString().endsWith(".")) {
-                binding.tvNumber.text = number.toInt().toString()
-                binding.tvResult.append(number.toInt().toString())
-            } else {
-                binding.tvNumber.text = number.toString()
-                binding.tvResult.append(number.toString())
-            }
-            NumbersFragment.number = number
-        }
-    }
-
-    fun onOperationClicked(operation: String) {
-        if (operation == "√") {
-            binding.tvResult.text = operation + binding.tvResult.text
-            result = if (binding.tvResult.text.toString().drop(1) != "")
-                binding.tvResult.text.toString().drop(1).toDouble()
-            else 0.0
-        } else {
-            binding.tvResult.append(operation)
-            result = if (binding.tvResult.text.toString().dropLast(1) != "")
-                binding.tvResult.text.toString().dropLast(1).toDouble()
-            else 0.0
-        }
-        binding.tvNumber.text = operation
-        NumbersFragment.operation = operation
-    }
-
-    fun onClearClicked() {
-        result = 0.0
-        number = 0.0
-        operation = ""
-        binding.tvResult.text = ""
-        binding.tvNumber.text = ""
-    }
-
-    fun onResultClicked() {
-        doMath()
-        number = result
-        if (number.toString().endsWith(".0") || number.toString().endsWith("."))
-            binding.tvResult.text = result.toInt().toString()
-        else
-            binding.tvResult.text = result.toString()
-        binding.tvNumber.text = ""
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentNumbersLinearBinding.bind(view)
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
         val list: List<Button> = listOf(
             binding.btn0,
             binding.btn1,
@@ -151,6 +59,12 @@ class NumbersFragment : Fragment(R.layout.fragment_numbers_constraint) {
         for (i in list) {
             i.setOnClickListener(Controller())
         }
+        mainViewModel.liveDataForResult.observe(viewLifecycleOwner) {
+            binding.tvResult.text = it
+        }
+        mainViewModel.liveDataForNumber.observe(viewLifecycleOwner) {
+            binding.tvNumber.text = it
+        }
     }
 
     override fun onCreateView(
@@ -166,9 +80,5 @@ class NumbersFragment : Fragment(R.layout.fragment_numbers_constraint) {
         fun newInstance(): NumbersFragment {
             return NumbersFragment()
         }
-
-        private var result: Double = 0.0
-        private var operation: String = ""
-        private var number: Double = 0.0
     }
 }
